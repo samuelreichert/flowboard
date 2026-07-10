1e46010e2a9d1ada8fef48925bfa5d8e8c88830b
Samuel Reichert <samuelreichertt@gmail.com>
2026-07-10 15:29:44 +0200

    fix: use refreshed icon in pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -45,6 +45,7 @@ export default defineConfig({
         'maskable-icon-dark-512x512.png',
       ],
       manifest: {
+        id: '/',
         name: 'Flowboard',
         short_name: 'Flowboard',
         description:
@@ -54,17 +55,17 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'pwa-light-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'pwa-light-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'maskable-icon-512x512.png',
+            src: 'maskable-icon-light-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
